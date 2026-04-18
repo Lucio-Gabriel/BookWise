6afafb47c97948f727692b741f517a4e4ce85ae5
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-04-18 10:39:45 -0300

    start project

---
+++ dados.php
@@ -0,0 +1,13 @@
+<?php
+
+$livros = [
+    ['id' => 1, 'titulo' => 'Dom Casmurro', 'autor' => 'Machado de Assis', 'descricao' => 'Romance clássico brasileiro que aborda ciúmes e memória.'],
+    ['id' => 2, 'titulo' => '1984', 'autor' => 'George Orwell', 'descricao' => 'Distopia sobre vigilância e controle estatal.'],
+    ['id' => 3, 'titulo' => 'O Hobbit', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Aventura fantástica sobre a jornada de Bilbo Bolseiro.'],
+    ['id' => 4, 'titulo' => 'A Revolução dos Bichos', 'autor' => 'George Orwell', 'descricao' => 'Sátira política em forma de fábula com animais.'],
+    ['id' => 5, 'titulo' => 'Harry Potter e a Pedra Filosofal', 'autor' => 'J.K. Rowling', 'descricao' => 'Início da jornada do jovem bruxo Harry Potter.'],
+    ['id' => 6, 'titulo' => 'O Pequeno Príncipe', 'autor' => 'Antoine de Saint-Exupéry', 'descricao' => 'História poética sobre amizade e sentido da vida.'],
+    ['id' => 7, 'titulo' => 'Senhor dos Anéis: A Sociedade do Anel', 'autor' => 'J.R.R. Tolkien', 'descricao' => 'Primeira parte da épica jornada pela Terra Média.'],
+    ['id' => 8, 'titulo' => 'Código Limpo', 'autor' => 'Robert C. Martin', 'descricao' => 'Boas práticas para escrever código de qualidade.'],
+    ['id' => 9, 'titulo' => 'Pai Rico, Pai Pobre', 'autor' => 'Robert Kiyosaki', 'descricao' => 'Lições sobre educação financeira e mentalidade.'],
+];
